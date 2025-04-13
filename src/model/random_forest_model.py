b8c59d85d5e4e24f3bf3158cec183ab4e957ed8c
Tim Liebhaber <timliebhaber@gmail.com>
2025-04-14 00:01:25 +0200

    refactor: update train_random_forest function to correctly utilize groups in LeaveOneGroupOut split and enhance prediction handling

--- src/model/random_forest_model.py
+++ src/model/random_forest_model.py
@@ -10,13 +10,13 @@ def train_random_forest(X, y, groups):
     y_true = []
     y_pred = []
     
-    for train_index, test_index in loo.split(X):
+    for train_index, test_index in loo.split(X, y, groups):
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
         y_train, y_test = y[train_index], y[test_index]
         model.fit(X_train, y_train)
         pred = model.predict(X_test)
-        y_true.append(y_test[0])
-        y_pred.append(pred[0])
+        y_true.extend(y_test)
+        y_pred.extend(pred)
     
     print("Accuracy RF: ", accuracy_score(y_true, y_pred))
     print("Classification Report RF:\n", classification_report(y_true, y_pred))
