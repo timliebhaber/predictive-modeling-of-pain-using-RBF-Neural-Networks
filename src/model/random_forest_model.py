07e6f252204dc08a14698927c3df12fc83be241f
Tim Liebhaber <timliebhaber@gmail.com>
2025-04-28 17:34:09 +0200

    refactor: rename function to train_random_forest_LOGO and add new training function

--- src/model/random_forest_model.py
+++ src/model/random_forest_model.py
@@ -3,7 +3,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import LeaveOneGroupOut
 
 
-def train_random_forest(X, y, groups):
+def train_random_forest_LOGO(X, y, groups):
     model = RandomForestClassifier(n_estimators=99, random_state=20)
     
     loo = LeaveOneGroupOut()
@@ -22,4 +22,31 @@ def train_random_forest(X, y, groups):
     print("Classification Report RF:\n", classification_report(y_true, y_pred))
     
     model.fit(X, y)
+    return model
+
+def train_random_forest(
+        X_train, X_test, y_train, y_test,
+        *,
+        n_estimators=100,
+        max_depth=None,
+        min_samples_leaf=1,
+        random_state=20,
+        refit_full=False
+    ):
+    
+    model = RandomForestClassifier(
+        n_estimators=n_estimators,
+        max_depth=max_depth,
+        min_samples_leaf=min_samples_leaf,
+        random_state=random_state
+    )
+
+    # Training
+    model.fit(X_train, y_train)
+
+    # Evaluation
+    y_pred = model.predict(X_test)
+    print("Accuracy RF:", accuracy_score(y_test, y_pred))
+    print("Classification Report RF:\n",
+          classification_report(y_test, y_pred))
     return model
